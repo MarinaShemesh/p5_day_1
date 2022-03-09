838f833f8301a6674ef8213dcf9fb6ed991f4417
Marina Shemesh <marina.shemesh1@gmail.com>
2022-03-09 22:53:45 +0200

    Add files via upload
    Learning about p5.js to do some creative coding. Day 1.

---
+++ sketch.js
@@ -0,0 +1,25 @@
+console.log('Hello Javascipt!');
+
+
+function setup(){
+    createCanvas(1350,600);
+    background (200);
+    
+
+//set colors
+fill (204,101,192,127);
+stroke (127,63,120);
+
+//rectangle
+    rect (100,20,70,70);    
+    rect (700,200,400,300);
+    rect (200,50,400,300);
+    rect (500,70,70,70);
+    rect (700,70,70,70);
+    rect (900,300,80,70);
+    rect (300,300,80,70);
+    rect (500,400,80,70);
+    rect (1200,400,80,70);
+}
+
+
